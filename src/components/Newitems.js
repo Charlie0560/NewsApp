e7069d4f064ddc65afb0a91fb5f957e0bbf6e8d8
Charlie0560 <chaitanyalokhande87@gmail.com>
2021-10-08 23:14:10 +0530

    added react-router-dom

--- src/components/Newitems.js
+++ src/components/Newitems.js
@@ -6,7 +6,7 @@ export class Newitems extends Component {
     let  {title , description , imageurl , newsUrl }=this.props
     return (
       <div className="my-3">
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card">
           <img src={!imageurl?"https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fillustrations%2Fnews-headlines-newsletter-636978%2F&psig=AOvVaw2FKObuTeurHLhv23BhK00d&ust=1633179535495000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNCE-IGiqfMCFQAAAAAdAAAAABAD":imageurl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,22 @@
 import React, { Component } from "react";
 import Newitems from "./Newitems";
+import Spinner from "./Spinner";
+import PropTypes from 'prop-types'
+
 
 export class News extends Component {
+  static defaultProps = {
+    country: 'us',
+    pageSize: 6,
+    category: 'general'
+  }
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  }
+
   constructor() {
     super();
     this.state = {
@@ -12,40 +27,48 @@ export class News extends Component {
   }
 
   async componentDidMount() {
-    let url =
-      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4fc263d3932a4935967ba79137231fbe&page=1&pageSize=20";
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=4fc263d3932a4935967ba79137231fbe&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       page: this.state.page,
       article: parsedData.articles,
       totalResults: parsedData.totalResults,
+      loadin: false
     });
   }
 
   handlePrevClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4fc263d3932a4935967ba79137231fbe&page=${
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=4fc263d3932a4935967ba79137231fbe&page=${
       this.state.page - 1
-    }&pageSize=20`;
+    }&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       page: this.state.page - 1,
       article: parsedData.articles,
+      loading: false,
     });
   };
   handleNextClick = async () => {
-    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
-    } else {
-      let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4fc263d3932a4935967ba79137231fbe&page=${
+    if (
+      !(
+        this.state.page + 1 >
+        Math.ceil(this.state.totalResults / this.props.pageSize)
+      )
+    ) {
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=4fc263d3932a4935967ba79137231fbe&page=${
         this.state.page + 1
-      }&pageSize=20`;
+      }&pageSize=${this.props.pageSize}`;
+      this.setState({ loading: true });
       let data = await fetch(url);
       let parsedData = await data.json();
       this.setState({
         page: this.state.page + 1,
         article: parsedData.articles,
-        
+        loading: false,
       });
     }
   };
@@ -54,8 +77,9 @@ export class News extends Component {
     return (
       <div className="container my-3">
         <h1 className="text-center">News app top headlines</h1>
+        {this.state.loading && <Spinner />}
         <div className="row">
-          {this.state.article.map((element) => {
+          {!this.state.loading && this.state.article.map((element) => {
             return (
               <div className="col-sm-4" key={element.url}>
                 <Newitems
@@ -79,7 +103,8 @@ export class News extends Component {
           </button>
           <button
             disabled={
-              this.state.page + 1 > Math.ceil(this.state.totalResults / 20)
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
             }
             type="button"
             className="btn btn-dark"
